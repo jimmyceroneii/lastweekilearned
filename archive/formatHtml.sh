5106bbf933992f668b6605d3d2b364fd7773067f
jimmyceroneii <jimmy.cerone@atomicobject.com>
2023-08-24 20:38:24 -0700

    remove unnecessary archive

--- archive/formatHtml.sh
+++
@@ -1,2 +0,0 @@
-sed -i "" '1s|^|<html>\n<head>\n<title>'"$2"'<\/title>\n<meta charset="utf-8">\n<link type="text/css" rel="stylesheet" href="style.css">\n<\/head>\n<body>\n|' "$1"
-echo "</body>\n</html>" >> "$1"
